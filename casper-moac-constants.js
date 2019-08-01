f62d46e56251d2d981e33458fa77740dc4ddae49
AndreSilva1993 <andreemanuel.1993@gmail.com>
2019-08-01 12:18:18 +0100

    Add casper-moac-constants file

---
+++ casper-moac-constants.js
@@ -0,0 +1,24 @@
+export const CasperMoacTypes = {
+  GRID: 'GRID',
+  GRID_EPAPER: 'GRID_EPAPER'
+};
+
+export const CasperMoacFilterTypes = {
+  PAPER_INPUT: 'PAPER_INPUT',
+  CASPER_SELECT: 'CASPER_SELECT',
+  CASPER_DATE_PICKER: 'CASPER_DATE_PICKER',
+};
+
+export const CasperMoacOperators = {
+  // String comparisons.
+  CONTAINS: 'CONTAINS',
+  ENDS_WITH: 'ENDS_WITH',
+  STARTS_WITH: 'STARTS_WITH',
+  EXACT_MATCH: 'EXACT_MATCH',
+  DOES_NOT_CONTAIN: 'DOES_NOT_CONTAIN',
+  // Numeric comparisons.
+  LESS_THAN: 'LESS_THAN',
+  GREATER_THAN: 'GREATER_THAN',
+  LESS_THAN_OR_EQUAL_TO: 'LESS_THAN_OR_EQUAL_TO',
+  GREATER_THAN_OR_EQUAL_TO: 'GREATER_THAN_OR_EQUAL_TO',
+}
