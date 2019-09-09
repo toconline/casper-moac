6860d642c81b27bf367d553d4d36a7777e6c4b2e
AndreSilva1993 <andreemanuel.1993@gmail.com>
2019-09-09 18:45:08 +0100

    Changed the default size of a resource's page

--- casper-moac-lazy-load-mixin.js
+++ casper-moac-lazy-load-mixin.js
@@ -12,7 +12,7 @@ export const CasperMoacLazyLoadMixin = superClass => {
          */
         resourcePageSize: {
           type: Number,
-          value: 250
+          value: 50
         },
         /**
          * The JSON API resource name that will be used to build the URL.
