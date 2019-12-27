58c8817698e45ab933e8f6101bcac7399342cca5
AndreSilva1993 <andreemanuel.1993@gmail.com>
2019-12-27 11:03:53 +0000

    Implements the reverse styling for the casper-moac-multi-selection-action

--- multi-selection/casper-moac-multi-selection-action.js
+++ multi-selection/casper-moac-multi-selection-action.js
@@ -9,6 +9,7 @@ class CasperMoacMultiSelectionAction extends PolymerElement {
     return {
       /**
        * Icon that will be used in the paper-button.
+       *
        * @type {String}
        */
       icon: {
@@ -16,12 +17,23 @@ class CasperMoacMultiSelectionAction extends PolymerElement {
       },
       /**
        * Flag that enables / disables the multi selection action.
+       *
        * @type {Boolean}
        */
       disabled: {
         type: Boolean,
         value: false,
         reflectToAttribute: true
+      },
+      /**
+       * Flag that enables / disables the reverse styling for the buttons where the primary color becomes the text color instead
+       * of the background.
+       *
+       * @type {Boolean}
+       */
+      reverse: {
+        type: Boolean,
+        value: false
       }
     };
   }
@@ -57,15 +69,25 @@ class CasperMoacMultiSelectionAction extends PolymerElement {
           background-color: var(--primary-color);
         }
 
-        #button:hover {
-          color: var(--primary-color);
-          background-color: var(--on-primary-color);
-        }
-
         #button[disabled] {
           pointer-events: none;
           color: var(--disabled-text-color);
           background-color: var(--disabled-background-color);
+        }
+
+        #button:not([disabled]):hover {
+          color: var(--primary-color);
+          background-color: var(--on-primary-color);
+        }
+
+        #button:not([disabled])[reverse] {
+          color: var(--primary-color);
+          background-color: var(--on-primary-color);
+        }
+
+        #button:not([disabled])[reverse]:hover {
+          color: var(--on-primary-color);
+          background-color: var(--primary-color);
         }
 
         #button casper-icon {
@@ -82,20 +104,28 @@ class CasperMoacMultiSelectionAction extends PolymerElement {
           color: var(--on-primary-color);
         }
 
-        #button:hover casper-icon {
+        #button:not([disabled]):hover casper-icon {
           color: var(--primary-color);
+        }
+
+        #button:not([disabled])[reverse] casper-icon {
+          color: var(--primary-color);
+        }
+
+        #button:not([disabled])[reverse]:hover casper-icon {
+          color: var(--on-primary-color);
         }
       </style>
 
       <template is="dom-if" if="[[!__onlyIcon]]">
-        <paper-button id="button" disabled="[[disabled]]">
+        <paper-button id="button" disabled="[[disabled]]" reverse$="[[reverse]]">
           <casper-icon icon="[[icon]]"></casper-icon>
           <slot></slot>
         </paper-button>
       </template>
 
       <template is="dom-if" if="[[__onlyIcon]]">
-        <casper-icon-button icon="[[icon]]" disabled="[[disabled]]"></casper-icon-button>
+        <casper-icon-button icon="[[icon]]" disabled="[[disabled]]" reverse="[[reverse]]"></casper-icon-button>
       </template>
     `;
   }
