cc827309acc9f79c6a34e00b8f51982d82d8aa0c
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-01-02 09:15:32 +0000

    casper-moac-multi-selection-action now uses the newly created casper-icon-button with text

--- multi-selection/casper-moac-multi-selection-action.js
+++ multi-selection/casper-moac-multi-selection-action.js
@@ -41,99 +41,19 @@ class CasperMoacMultiSelectionAction extends PolymerElement {
   static get template () {
     return html`
       <style>
-        :host {
-          height: 25px;
-          margin-top: 10px;
-          margin-right: 5px;
-        }
-
-        :host([disabled]) {
-          cursor: not-allowed;
-          pointer-events: none;
-        }
-
         casper-icon-button {
           width: 25px;
           height: 25px;
           padding: 5px;
-        }
-
-        #button {
-          margin: 0;
-          height: 25px;
-          font-size: 10px;
-          font-weight: bold;
-          padding: 2px 8px;
-          border-radius: 15px;
-          color: var(--on-primary-color);
-          background-color: var(--primary-color);
-        }
-
-        #button[disabled] {
-          pointer-events: none;
-          color: var(--disabled-text-color);
-          background-color: var(--disabled-background-color);
-        }
-
-        #button:not([disabled]):hover {
-          color: var(--primary-color);
-          background-color: var(--on-primary-color);
-        }
-
-        #button:not([disabled])[reverse] {
-          color: var(--primary-color);
-          background-color: var(--on-primary-color);
-        }
-
-        #button:not([disabled])[reverse]:hover {
-          color: var(--on-primary-color);
-          background-color: var(--primary-color);
-        }
-
-        #button casper-icon {
-          width: 15px;
-          height: 15px;
-          margin-right: 5px;
-        }
-
-        #button[disabled] casper-icon {
-          color: var(--disabled-text-color);
-        }
-
-        #button:not([disabled]) casper-icon {
-          color: var(--on-primary-color);
-        }
-
-        #button:not([disabled]):hover casper-icon {
-          color: var(--primary-color);
-        }
-
-        #button:not([disabled])[reverse] casper-icon {
-          color: var(--primary-color);
-        }
-
-        #button:not([disabled])[reverse]:hover casper-icon {
-          color: var(--on-primary-color);
+          font-size: 12px;
+          margin: 10px 2px 0;
         }
       </style>
 
-      <template is="dom-if" if="[[!__onlyIcon]]">
-        <paper-button id="button" disabled="[[disabled]]" reverse$="[[reverse]]">
-          <casper-icon icon="[[icon]]"></casper-icon>
-          <slot></slot>
-        </paper-button>
-      </template>
-
-      <template is="dom-if" if="[[__onlyIcon]]">
-        <casper-icon-button icon="[[icon]]" disabled="[[disabled]]" reverse="[[reverse]]"></casper-icon-button>
-      </template>
+      <casper-icon-button icon="[[icon]]" disabled="[[disabled]]" reverse="[[reverse]]">
+        <slot></slot>
+      </casper-icon-button>
     `;
-  }
-
-  ready () {
-    super.ready();
-
-    this.__onlyIcon = this.childNodes.length === 0;
   }
 }
 
