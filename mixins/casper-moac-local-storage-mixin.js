b676de2d75e5b81173e023f0264f31476a09dd5a
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-03-25 09:36:10 +0000

    Adds missing local storage mixin file

---
+++ mixins/casper-moac-local-storage-mixin.js
@@ -0,0 +1,42 @@
+export const CasperMoacLocalStorageMixin = superClass => {
+  return class extends superClass {
+
+    static get properties () {
+      return {
+        /**
+         * This property sets the local storage key that will be used to store the filters.
+         *
+         * @type {String}
+         */
+        localStorageKey: {
+          type: String,
+          value: 'teste'
+        }
+      }
+    }
+
+    /**
+     * Retrieves the filters information that was previously save in the local storage.
+     */
+    __retrieveFiltersFromLocalStorage () {
+      const localStorageFilters = window.localStorage.getItem(this.localStorageKey);
+
+      return JSON.parse(localStorageFilters) || {};
+    }
+
+    /**
+     * Saves the current filters information into the local storage.
+     */
+    __saveFiltersIntoLocalStorage () {
+      const localStorageFilters = {};
+
+      Object.entries(this.filters).forEach(([filterKey, filter]) => {
+        if (this.__valueIsNotEmpty(filter.value)) {
+          localStorageFilters[filterKey] = filter.value;
+        }
+      });
+
+      window.localStorage.setItem(this.localStorageKey, JSON.stringify(localStorageFilters));
+    }
+  }
+};
