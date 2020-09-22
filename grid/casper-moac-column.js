880132d6f1fa64ad2a9a4ab5b3d67d958bbfb978
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-09-22 17:27:33 +0100

    Creates a casper-moac-column which extends the vaadin-grid-column and for now allows tooltips on its header

---
+++ grid/casper-moac-column.js
@@ -0,0 +1,36 @@
+import { html } from '@polymer/polymer/polymer-element.js';
+import { GridColumnElement } from '@vaadin/vaadin-grid/src/vaadin-grid-column.js';
+
+class CasperMoacColumn extends GridColumnElement {
+
+  static get properties () {
+    return {
+      /**
+       * The header's tooltip.
+       *
+       * @type {String}
+       */
+      tooltip: String,
+    }
+  }
+
+  static get template () {
+    return html`
+      <template class="header">
+        <div tooltip$="[[tooltip]]">[[header]]</div>
+      </template>
+    `;
+  }
+
+  /**
+   * Method invoked from the vaadin grid itself to stamp the template and bind the dataHost.
+   */
+  _prepareHeaderTemplate () {
+    const headerTemplate = this._prepareTemplatizer(this.shadowRoot.querySelector('template'));
+    headerTemplate.templatizer.dataHost = this;
+
+    return headerTemplate;
+  }
+}
+
+window.customElements.define('casper-moac-column', CasperMoacColumn);
