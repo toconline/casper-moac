f0f120b1891b27733b009024eb3677f1312477fa
Rita Silva <arita.lcas@gmail.com>
2021-04-30 12:16:38 +0100

    Cleaned up code

--- grid/casper-moac-toggle-column.js
+++ grid/casper-moac-toggle-column.js
@@ -1,44 +1,10 @@
-import '@cloudware-casper/casper-icons/casper-icon.js';
 import { html } from '@polymer/polymer/polymer-element.js';
 import { GridColumnElement } from '@vaadin/vaadin-grid/src/vaadin-grid-column.js';
-import { CasperMoacSortTypes, CasperMoacSortDirections } from '../casper-moac-constants';
 
 class CasperMoacToggleColumn extends GridColumnElement {
 
   static get properties () {
     return {
-      /**
-       * This property specifies the column data type so that the component knows how to sort the items.
-       *
-       * @type {String}
-       */
-      dataType: {
-        type: String,
-        value: CasperMoacSortTypes.STRING
-      },
-      /**
-       * This property when present overrides the path property when querying the database.
-       *
-       * @type {String}
-       */
-      databaseField: String,
-      /**
-       * This property states the direction in which the items are being sorted, either ascending
-       * or deescending.
-       *
-       * @type {String}
-       */
-      direction: {
-        type: String,
-        notify: true,
-        value: null
-      },
-      /**
-       * If there are multiple sorters being applied, this states its order / priority.
-       *
-       * @type {Number}
-       */
-      sortOrder: Number,
       /**
        * The header's tooltip.
        *
@@ -46,70 +12,54 @@ class CasperMoacToggleColumn extends GridColumnElement {
        */
       tooltip: String,
       /**
-       * First header's title.
+       * First button's text.
        *
        * @type {String}
        */
-      header1: String,
+      button1: String,
       /**
-       * Second header's title.
+       * Second button's text.
        *
        * @type {String}
        */
-      header2: String,
+      button2: String,
       /**
-       * First header's title class.
+       * First button's class.
        * 
        * @type {String}
        */
-      header1Class: {
+      firstButtonClass: {
         type: String,
-        value: 'header-title header-toggle-title selected-header-toggle-title'
+        value: 'header-title toggle-button selected-toggle-button'
       },
       /**
-       * Second header's title class.
+       * Second button's class.
        * 
        * @type {String}
        */
-       header2Class: {
+       secondButtonClass: {
         type: String,
-        value: 'header-title header-toggle-title'
+        value: 'header-title toggle-button'
       },
       /**
-       * The header's selected button/title.
+       * Selected button.
        * 
        * @type {String}
        */
-      selectedButton: {
+       selectedButton: {
         type: String,
-        value: 'first'
-      }
+        value: 'first-button'
+       }
     }
   }
 
   static get template () {
     return html`
       <template class="header">
-        <!-- <div
-          tooltip$="[[tooltip]]"
-          on-click="__toggleDirection"
-          class="casper-moac-sort-column"
-          style="[[__getHeaderContainerAlignment()]]">
-          <span class="header-title">[[header]]</span>
-
-          <div class="header-sort">
-            <casper-icon icon="[[__getIcon(direction)]]" style="[[__getIconOpacity(direction)]]"></casper-icon>
-
-            <template is="dom-if" if="[[sortOrder]]">
-              <span class="header-sort-order">[[sortOrder]]</span>
-            </template>
-          </div>
-        </div> -->
-
-        <div tooltip$="[[tooltip]]" class="casper-moac-sort-column" style="[[__getHeaderContainerAlignment()]]" selected$="[[selectedButton]]">
-          <div class="header-toggle-father">
-            <span id="first-title" class$="[[header1Class]]" on-click="__toggleClass">[[header1]]</span>
-            <span id="second-title" class$="[[header2Class]]" on-click="__toggleClass">[[header2]]</span>
+        <div tooltip$="[[tooltip]]" class="casper-moac-sort-column" style="[[__getHeaderContainerAlignment()]]">
+          <div class="toggle-buttons-container">
+            <span id="first-button" class$="[[firstButtonClass]]" on-click="__toggleButton">[[button1]]</span>
+            <span id="second-button" class$="[[secondButtonClass]]" on-click="__toggleButton">[[button2]]</span>
           </div>
         </div>
       </template>
@@ -127,46 +77,6 @@ class CasperMoacToggleColumn extends GridColumnElement {
   }
 
   /**
-   * This method is invoked when the user clicks on the sort column header which shall change its direction.
-   *
-   * @param {Object} event The event's object.
-   */
-  __toggleDirection (event) {
-    const directions = [null, CasperMoacSortDirections.ASCENDING, CasperMoacSortDirections.DESCENDING];
-    const currentDirection = directions.findIndex(direction => direction === this.direction);
-
-    this.direction = currentDirection + 1 < directions.length
-      ? directions[currentDirection + 1]
-      : directions[0];
-
-    // Manipulate the icon and opacity.
-    this.__headerIcon = this.__headerIcon || event.target.closest('.casper-moac-sort-column').querySelector('casper-icon');
-    this.__headerIcon.icon = this.__getIcon(this.direction);
-    this.__headerIcon.style = this.__getIconOpacity(this.direction);
-  }
-
-  /**
-   * This method returns the casper-icon depending on the current direction of the sorter.
-   */
-  __getIcon (direction) {
-    switch (direction) {
-      case CasperMoacSortDirections.ASCENDING:
-        return 'fa-solid:sort-up';
-      case CasperMoacSortDirections.DESCENDING:
-        return 'fa-solid:sort-down';
-      default:
-        return 'fa-solid:sort';
-    }
-  }
-
-  /**
-   * This method returns the casper-icon's opacity depending on the current direction of the sorter.
-   */
-  __getIconOpacity (direction) {
-    return `opacity: ${!direction ? '0.2' : '1'}`;
-  }
-
-  /**
    * This method returns the styling required to horizontally align the title.
    */
   __getHeaderContainerAlignment () {
@@ -178,29 +88,30 @@ class CasperMoacToggleColumn extends GridColumnElement {
   }
 
   /**
-   * This method 
+   * This method is called when the user clicks on a toggle button. 
+   * It sets the correct classes and dispatches a new event that can be listened to elsewhere.
    */
-  __toggleClass (event) {
-    if (event && event.currentTarget) {
-      let initialClass = 'header-title header-toggle-title';
+  __toggleButton (event) {
+    if (event && event.currentTarget && event.currentTarget.id) {
+      const targetId = event.currentTarget.id;
+      const initialClass = 'header-title toggle-button';
 
-      if (event.currentTarget.id === 'first-title') {
-        this.header1Class = `${initialClass} selected-header-toggle-title`;
-        this.header2Class = initialClass;
-  
-        this.selectedButton = 'first';
-      } else if (event.currentTarget.id === 'second-title') {
-        this.header2Class = `${initialClass} selected-header-toggle-title`;
-        this.header1Class = initialClass;
-  
-        this.selectedButton = 'second';
+      if (targetId === 'first-button') {
+        this.firstButtonClass = `${initialClass} selected-toggle-button`;
+        this.secondButtonClass = initialClass;
+      } else if (targetId === 'second-button') {
+        this.secondButtonClass = `${initialClass} selected-toggle-button`;
+        this.firstButtonClass = initialClass;
       }
+
+      this.selectedButton = targetId;
+
       event.stopImmediatePropagation();
   
-      this.dispatchEvent(new CustomEvent('casper-moac-toggle', {
+      this.dispatchEvent(new CustomEvent('casper-moac-toggle-column-toggle', {
         bubbles: true,
         composed: true,
-        detail: { target_id: event.currentTarget.id }
+        detail: { target_id: targetId }
       }));
     }
   }
