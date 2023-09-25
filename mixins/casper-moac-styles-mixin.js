9ad9a2a7efb6a6fbb85a7ffbd8f3e5330f512976
João Ramos <joao.ramos@cldware.com>
2023-09-25 14:13:13 +0100

    set the no-data casper-icon use values from theme.css

--- mixins/casper-moac-styles-mixin.js
+++ mixins/casper-moac-styles-mixin.js
@@ -394,8 +394,8 @@ export const CasperMoacStylesMixin = superClass => {
             border-radius: var(--no-grid--icon--border-radius);
             border: var(--no-grid--icon--border);
 
-            width: 100px;
-            height: 100px;
+            width: var(--no-grid--icon--width, '100px');
+            height: var(--no-grid--icon--height, '100px');
             margin-bottom: 25px;
             color: var(--no-grid--icon--color, var(--status-gray));
           }
